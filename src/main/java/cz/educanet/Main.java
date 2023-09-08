5385d32eb31007c74cf623ac4d899d51d376915d
Jakub Tesař <73097855+JakubTesar@users.noreply.github.com>
2023-09-08 08:32:58 +0200

    neco neco determinant 2x2 a 3x3, 4x4  je peklo, nevím jak nevím proč

--- src/main/java/cz/educanet/Main.java
+++ src/main/java/cz/educanet/Main.java
@@ -2,6 +2,16 @@ package cz.educanet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // sloupce
+        double[][] a = {{2,3,1},
+                        {1,2,4}};
+        // řádky
+        double[][] b = {{1,0},
+                        {0,1},
+                        {0,1}};
+
+        System.out.println(a[1][0]);
+
     }
 }
--- src/main/java/cz/educanet/matrices/Matrix.java
+++ src/main/java/cz/educanet/matrices/Matrix.java
@@ -1,8 +1,11 @@
 package cz.educanet.matrices;
 
+import java.lang.reflect.Array;
+
 public class Matrix implements IMatrix {
 
     private final double[][] rawArray;
+    private final IMatrixFactory matrixFactory = MatrixFactory.instance;
 
     public Matrix(double[][] rawArray) {
         this.rawArray = rawArray;
@@ -13,31 +16,46 @@ public class Matrix implements IMatrix {
      */
     @Override
     public IMatrix times(IMatrix matrix) {
+        Matrix a = new Matrix(rawArray);
+        Matrix b = (Matrix) matrix;
+
+        double[][] c = new double[a.getRows()][b.getColumns()];
+        if (a.getColumns() != b.getRows())
+            throw new IllegalArgumentException();
+
+        for (int i = 0; i < c; i++) {
+
+        }
         return null;
     }
 
-    /**
-     * TODO: Implement
-     */
     @Override
     public IMatrix times(Number scalar) {
-        return null;
+        double[][] timedMatrix = rawArray;
+        for (int i = 0; i < timedMatrix.length; i++)
+            for (int j = 0; j < timedMatrix.length; j++)
+                timedMatrix[i][j] *= (int) scalar;
+        return new Matrix(timedMatrix);
     }
 
-    /**
-     * TODO: Implement
-     */
     @Override
     public IMatrix add(IMatrix matrix) {
-        return null;
+        double[][] timedMatrix = rawArray;
+        Matrix matrix1 = (Matrix) matrix;
+        for (int i = 0; i < timedMatrix.length; i++)
+            for (int j = 0; j < timedMatrix.length; j++)
+                timedMatrix[i][j] += matrix.get(i, j);
+        return new Matrix(timedMatrix);
     }
 
-    /**
-     * TODO: Implement
-     */
     @Override
     public IMatrix transpose() {
-        return null;
+        double[][] inputMatrix = rawArray;
+        double[][] tMatrix = rawArray;
+        for (int i = 0; i < inputMatrix.length; i++)
+            for (int j = 0; j < inputMatrix.length; j++)
+                tMatrix[i][j] = inputMatrix[j][i];
+        return new Matrix(tMatrix);
     }
 
     /**
@@ -45,31 +63,47 @@ public class Matrix implements IMatrix {
      */
     @Override
     public double determinant() {
-        return 0;
+        if (isSquare()) {
+            double[][] a = rawArray;
+            if (a.length == 1)
+                return a[0][0];
+            if (a.length == 2)
+                return ((a[0][0] * a[1][1]) - (a[1][0] * a[0][1]));
+            if (a.length == 3)
+                return ((a[0][0] * a[1][1] * a[2][2] + a[0][1] * a[1][2] * a[2][1] + a[0][2] * a[1][1] * a[2][2]) - (a[2][0] * a[1][1] * a[0][2] + a[2][1] * a[1][2] * a[0][1] + a[2][2] * a[1][1] * a[0][2]));
+            if (a.length == 4) {
+
+            }
+            return 0;
+        } else
+            throw new RuntimeException("Matice není čtvercová");
     }
 
-    /**
-     * TODO: Implement
-     */
     @Override
     public boolean isSquare() {
-        return false;
+        return rawArray.length == rawArray[0].length;
     }
 
-    /**
-     * TODO: Implement
-     */
     @Override
     public boolean isDiagonal() {
-        return false;
+        if (isSquare()) {
+            double[][] matrixDiagonal = new double[rawArray.length][rawArray[0].length];
+            for (int i = 0; i < rawArray.length; i++)
+                matrixDiagonal[i][i] = rawArray[i][i];
+            return matrixDiagonal.equals(rawArray);
+        } else
+            throw new RuntimeException("Matice není čtvercová");
     }
 
-    /**
-     * TODO: Implement
-     */
     @Override
     public Number getTrace() {
-        return null;
+        if (isSquare()) {
+            double sum = 0;
+            for (int i = 0; i < rawArray.length; i++)
+                sum += rawArray[i][i];
+            return sum;
+        } else
+            throw new RuntimeException("Matice není čtvercová");
     }
 
     @Override
@@ -87,7 +121,7 @@ public class Matrix implements IMatrix {
 
     @Override
     public double get(int n, int m) {
-        if(n >= getRows() || n < 0 || m >= getColumns() || m < 0)
+        if (n >= getRows() || n < 0 || m >= getColumns() || m < 0)
             throw new IllegalArgumentException();
 
         return rawArray[n][m];
